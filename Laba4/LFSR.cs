15763ef2e0cb9974e7084776d1ec5b4c3d9d23a2
SultKham21 <khamroevsulton2001@gmail.com>
2021-05-29 10:03:58 +0300

    labs

--- Laba4/LFSR.cs
+++ Laba4/LFSR.cs
@@ -53,9 +53,9 @@ namespace lab_4
         //x^24 + x^2 + 1
         public static ulong Key()
         {
-            ulong bit_28 = (register >> 23) & 1;
+            ulong bit_24 = (register >> 23) & 1;
             ulong bit_1 = (register >> 1) & 1;
-            ulong xor_bits = bit_28 ^ bit_1;
+            ulong xor_bits = bit_24 ^ bit_1;
 
             register = (xor_bits << 31) | (register >> 1);
             return register & 1;
